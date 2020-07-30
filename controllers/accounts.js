35d1b97c1cc7321e654c6ed0a331499aec435a90
Glitch (thread-bumpy-rifle) <none>
2020-07-30 09:46:52 +0000

    🎃📚 Checkpoint ./controllers/accounts.js:17820309/67

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -43,11 +43,11 @@ const accounts = {
 
   authenticate(request, response) {
     const user = userstore.getUserByEmail(request.body.email);
-    const tUser = trainerstore.getTrainerByEmail(request.body.email);
+    const trainer = trainerstore.getTrainerByEmail(request.body.email);
     if (user) {
       response.cookie('member', user.email);
       logger.info(`logging in ${user.email}`);
-      response.redirect('/dashboard');
+      response.redirect('/member/:id');
     }
     else if (trainer){      
       response.cookie('trainer', trainer.email);
