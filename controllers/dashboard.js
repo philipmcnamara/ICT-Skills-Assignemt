c56a058c773523fd11d4a216b109716da5ad1953
Glitch (thread-bumpy-rifle) <none>
2020-08-05 13:28:19 +0000

    🌧🏁 Checkpoint ./controllers/dashboard.js:17820309/3540

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,10 +5,8 @@ const logger = require("../utils/logger");
 const trainerStore = require('../models/trainer-store');
 const accounts = require ('./accounts.js');
 const userStore = require('../models/member-store');
-
-
  
-const dashboard = {
+  const dashboard = {
     index(request, response) {
     logger.info('dashboard rendering');
     const loggedInUser = accounts.getCurrentUser(request);
@@ -19,7 +17,23 @@ const dashboard = {
       member: userStore.getAllUsers(loggedInUser.id),
     };
     logger.info('Trainer members:', userStore.getUser(loggedInUser.id));
-    response.render('dashboard', viewData);
+
+    const userEmail = request.cookies.member;
+    const trainerEmail = request.cookies.trainer;
+    if(userEmail != "")
+      {
+        logger.info(`User Email returned from getCurrentUser: ${userEmail}`);
+        response.cookie('member', loggedInUser.email);
+        logger.info(`logging in member ${loggedInUser.email}`);
+        logger.info(`logging in member ${loggedInUser.id}`);
+        response.redirect('/member/'+ loggedInUser.id);
+        //response.render('userDashboard', viewData);
+      }
+    else
+      {
+        logger.info(`Trainer Email returned from getCurrentUser: ${trainerEmail}`);
+        response.render('dashboard', viewData);
+      }
   },
 
   
