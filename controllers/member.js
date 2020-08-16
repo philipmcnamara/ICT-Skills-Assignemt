481a540c508a5fd495cdaff1f81ed1130b2081e9
Glitch (thread-bumpy-rifle) <none>
2020-08-16 20:33:00 +0000

    👕🎾 Checkpoint ./controllers/member.js:17820309/2398 ./models/members.json:17820309/29 ./views/partials/liststats.hbs:17820309/118

--- controllers/member.js
+++ controllers/member.js
@@ -146,8 +146,33 @@ const member = {
           logger.info(`lostWeight being true :  ${lostWeight}`);
       }*/
     logger.info(`Lost Weight :  ${lostWeight}`);
+      
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; 
+      var yyyy = today.getFullYear();
+      if(dd<10) 
+      {
+          dd='0'+dd;
+      } 
+
+      if(mm<10) 
+      {
+          mm='0'+mm;
+      } 
+      today = mm+'-'+dd+'-'+yyyy;
+      console.log(today);
+      today = mm+'/'+dd+'/'+yyyy;
+      console.log(today);
+      today = dd+'-'+mm+'-'+yyyy;
+      console.log(today);
+      today = dd+'/'+mm+'/'+yyyy;
+      console.log(today);
+      
+      
 
     const newStat = {
+      today: today,
       statId: uuid.v1(),
       weight: request.body.weight,
       chest: request.body.chest,
