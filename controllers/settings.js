4f4ecb0c3a61ce50c01e17307e238bca033e7527
Glitch (thread-bumpy-rifle) <none>
2020-08-19 18:30:15 +0000

    🏊🏼💏 Checkpoint ./controllers/settings.js:17820309/282 ./views/settings.hbs:17820309/396

--- controllers/settings.js
+++ controllers/settings.js
@@ -15,8 +15,10 @@ const settings = {
 
   updateMember(request, response) {
     const memberId = request.params.id;
+    const member = userStore.getUser(memberId);
+    logger.info(`8888888888888888888888888888888888888888 ${member.name} ${member.gender}`);
     
-    const updateMember = {   
+    const memberChange = {   
       
       name: request.body.name,
       gender: request.body.gender,
@@ -25,8 +27,9 @@ const settings = {
       height: request.body.height,
       startingWeight: request.body.startingWeight,
     };
+    
     logger.debug(`Updating Member ${memberId}`);
-    userStore.updateUser( updateMember);
+    userStore.updateUser( memberChange, memberId);
     response.redirect("/settings/" + memberId);
   }
 };
