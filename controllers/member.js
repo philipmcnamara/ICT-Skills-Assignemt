6f52568d8d08e566279dde54dd16d58900ec11b7
Glitch (thread-bumpy-rifle) <none>
2020-08-19 10:21:02 +0000

    🎣💋 Checkpoint ./controllers/member.js:17820309/440

--- controllers/member.js
+++ controllers/member.js
@@ -151,24 +151,19 @@ const member = {
       var dd = today.getDate();
       var mm = today.getMonth()+1; 
       var yyyy = today.getFullYear();
+      
       if(dd<10) 
-      {
+        {
           dd='0'+dd;
-      } 
+        } 
 
       if(mm<10) 
-      {
+        {
           mm='0'+mm;
-      } 
-      today = mm+'-'+dd+'-'+yyyy;
-      console.log(today);
-      today = mm+'/'+dd+'/'+yyyy;
-      console.log(today);
-      today = dd+'-'+mm+'-'+yyyy;
-      console.log(today);
+        } 
+
       today = dd+'/'+mm+'/'+yyyy;
-      console.log(today);
-      
+
       
 
     const newStat = {
